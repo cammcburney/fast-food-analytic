f1621e864462ba6ceba7cb888d4b4c7e623f655e
cammcburney <cammcburney95@gmail.com>
2024-06-06 16:30:29 +0100

    Creates test file for ingestion

--- testing/test_db_ingestion.py
+++ testing/test_db_ingestion.py
@@ -1,5 +1,5 @@
 import pytest
-from src.python.db_ingestion import (read_data_file_into_dataframe,
+from python.ingestion_utils import (read_data_file_into_dataframe,
                                      insert_data_into_database)
 
 class Test_Read_Data_File_Into_Dataframe:
