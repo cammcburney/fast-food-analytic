8e56443704c43661270779579e0d9026473e815c
lUKEdOWNEY <lukedowney2014.ld@gmail.com>
2024-06-11 14:45:20 +0100

    add more SQL queries to be visualised

--- src/queries/queries.sql
+++ src/queries/queries.sql
@@ -51,3 +51,54 @@ GROUP BY
     TO_CHAR(DATE_TRUNC('month', TO_DATE(f.date, 'DD/MM/YYYY')), 'Month')
 ORDER BY 
      month, m.manager DESC;
+
+"""
+AVG PROFIT PER UNIT
+"""
+
+SELECT AVG(profit/quantity)
+AS  Avg_Profit_Per_Unit
+FROM fact;
+
+"""
+SALES BY PAYMENT METHOD
+"""
+
+SELECT
+    payment_method_id, COUNT(order_id)
+AS Total_Sales,
+    SUM(revenue)
+AS Total_Revenue,
+FROM fact
+GROUP BY payment_method_id 
+ORDER BY Total_Sales 
+DESC;
+
+"""
+PRODUCT PERFORMANCE ANALYSIS
+"""
+
+SELECT  
+    product_id,
+    SUM(revenue)
+AS Total_Revenue,
+    SUM(profit)
+AS Total_Profit
+FROM fact
+GROUP BY product_id
+ORDER BY Total_Profit;
+
+"""
+REVENUE AND PROFIT BY PURCHASE TYPES
+"""
+
+SELECT
+    purchase_type_id,
+    SUM(revenue)
+AS Total_Revenue,
+    SUM(profit)
+AS Total_Profit
+FROM fact
+GROUP By purchase_type_id
+ORDER BY Total_Revenue
+DESC;
