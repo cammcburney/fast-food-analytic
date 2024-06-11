9897f2aa9c3370dc0561f091ee83654d6bb261a6
cammcburney <cammcburney95@gmail.com>
2024-06-11 14:19:52 +0100

    Updates queries for adjusted database format

--- src/queries/queries.sql
+++ src/queries/queries.sql
@@ -3,41 +3,51 @@ This query gathers total revenue and profit for the last monthly by week.
 """
 
 SELECT
-    TO_CHAR(DATE_TRUNC('week', TO_DATE(f."Date", 'DD/MM/YYYY')), 'YYYY/MM/DD') AS week,
-    ROUND(SUM(f."Revenue")::numeric , 2) AS total_revenue,
-    ROUND(SUM(f."Profit")::numeric, 2) AS total_profit
+    TO_CHAR(DATE_TRUNC('week', TO_DATE(f.date, 'DD/MM/YYYY')), 'YYYY/MM/DD') AS week,
+    ROUND(SUM(f.revenue)::numeric , 2) AS total_revenue,
+    ROUND(SUM(f.profit)::numeric, 2) AS total_profit
 FROM 
     fact f
 GROUP BY 
-    TO_CHAR(DATE_TRUNC('week', TO_DATE(f."Date", 'DD/MM/YYYY')), 'YYYY/MM/DD')
+    TO_CHAR(DATE_TRUNC('week', TO_DATE(f.date, 'DD/MM/YYYY')), 'YYYY/MM/DD')
 ORDER BY 
     week DESC
 LIMIT 4;
 
+"""
+This query gathers total revenue and profit for each month per year
+"""
 
 SELECT
-    TO_CHAR(DATE_TRUNC('year', TO_DATE(f."Date", 'DD/MM/YYYY')), 'YYYY') AS year,
-    TO_CHAR(DATE_TRUNC('month', TO_DATE(f."Date", 'DD/MM/YYYY')), 'Month') AS month,
-    ROUND(SUM(f."Revenue")::numeric, 2) AS total_revenue,
-    ROUND(SUM(f."Profit")::numeric, 2) AS total_profit
+    TO_CHAR(DATE_TRUNC('year', TO_DATE(f.date, 'DD/MM/YYYY')), 'YYYY') AS year,
+    TO_CHAR(DATE_TRUNC('month', TO_DATE(f.date, 'DD/MM/YYYY')), 'Month') AS month,
+    ROUND(SUM(f.revenue)::numeric, 2) AS total_revenue,
+    ROUND(SUM(f.profit)::numeric, 2) AS total_profit
 FROM 
     fact f
 GROUP BY 
-    TO_CHAR(DATE_TRUNC('year', TO_DATE(f."Date", 'DD/MM/YYYY')), 'YYYY'),
-    TO_CHAR(DATE_TRUNC('month', TO_DATE(f."Date", 'DD/MM/YYYY')), 'Month')
+    TO_CHAR(DATE_TRUNC('year', TO_DATE(f.date, 'DD/MM/YYYY')), 'YYYY'),
+    TO_CHAR(DATE_TRUNC('month', TO_DATE(f.date, 'DD/MM/YYYY')), 'Month')
 ORDER BY 
     year DESC;
 
 
+"""
+This query gathers total revenue and profit for manager per month by year.
+"""
+
 SELECT
-    TO_CHAR(DATE_TRUNC('month', TO_DATE(f."Date", 'DD/MM/YYYY')), 'Month') AS month,
-    ROUND(SUM(f."Revenue")::numeric , 2) AS total_revenue,
-    ROUND(SUM(f."Profit")::numeric, 2) AS total_profit,
-    m."Manager"
+    TO_CHAR(DATE_TRUNC('year', TO_DATE(f.date, 'DD/MM/YYYY')), 'YYYY') AS year,
+    TO_CHAR(DATE_TRUNC('month', TO_DATE(f.date, 'DD/MM/YYYY')), 'Month') AS month,
+    ROUND(SUM(f.revenue)::numeric , 2) AS total_revenue,
+    ROUND(SUM(f.profit)::numeric, 2) AS total_profit,
+    m.manager
 FROM 
     fact f
-INNER JOIN manager m ON m."Manager_id" = f."manager_id"
+INNER JOIN manager m ON m.manager_id = f.manager_id
 GROUP BY 
-    m."Manager", TO_CHAR(DATE_TRUNC('month', TO_DATE(f."Date", 'DD/MM/YYYY')), 'Month')
+    TO_CHAR(DATE_TRUNC('year', TO_DATE(f.date, 'DD/MM/YYYY')), 'YYYY'),
+    m.manager, 
+    TO_CHAR(DATE_TRUNC('month', TO_DATE(f.date, 'DD/MM/YYYY')), 'Month')
 ORDER BY 
-     month, m."Manager" DESC;
+     month, m.manager DESC;
